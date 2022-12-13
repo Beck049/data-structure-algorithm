16412ffbdf0034a14769c206580d9c4e57965c82
Beck049 <beckxiao2001323@gmail.com>
2022-12-13 10:15:17 +0800

    fix: Dynamic array - small bugs

--- 00.Array/Dynamic_Array.cpp
+++ 00.Array/Dynamic_Array.cpp
@@ -9,19 +9,24 @@
 #include <string.h>	
 #include <stdint.h>
 
+// g++ Dynamic_Array.cpp -o da
+
 void print  (int* , int);
+int* create (int* , int);
 int  add    (int*, int, int);
 int  search (int*, int, int);
-int  delete (int*, int, int);
+int  delete_ele (int*, int, int);
 int  append (int*, int*, int, int);
 
 int main()
 {
     int len1 = 0, len2 = 0;
-    int *arr1 = (int*)malloc(sizeof(int) *len1);
-    int *arr2 = (int*)calloc(sizeof(int), len2);
-
+    printf("init array1: ");
+    int* arr1 = (int*)malloc(sizeof(int) *len1);
     print(arr1, len1);
+    
+    printf("\ninit array2: ");
+    int* arr2 = (int*)calloc(sizeof(int), len2);
     print(arr2, len2);
 
     len1 = add(arr1, len1, 23);
@@ -30,19 +35,30 @@ int main()
     len2 = add(arr2, len2, 56);
     len2 = add(arr2, len2, 67);
     
+    printf("\n\narray 1: ");
     print(arr1, len1);
+    printf("\narray 2: ");
     print(arr2, len2);
 
+    printf("\n\nsearch 34 in array 1: ");
     search(arr1, len1, 34);
+    printf("\nsearch array 2 in array 2:");
     search(arr2, len2, 34);
 
-    len2 = delete(arr2, len2, 45);
+    len2 = delete_ele(arr2, len2, 45);
+    printf("\n\ndelete 45 in array 2: ");
     print(arr2, len2);
 
     len1 = append(arr1, arr2, len1, len2);
+    printf("\n\nappend array 2 after array 1: \n");
     print(arr1, len1);
 
     return 0;
+}
+
+int* create(int* arr, int len){
+    arr = (int*)malloc(sizeof(int) *len);
+    return arr;
 }
 
 int  append (int* arr1, int* arr2, int len1, int len2)
@@ -52,14 +68,14 @@ int  append (int* arr1, int* arr2, int len1, int len2)
         return len1;
     }
 
-    arr1 = realloc( arr1 , sizeof(int) * (len1 + len2));
+    arr1 = (int*)realloc( arr1 , sizeof(int) * (len1 + len2));
     memcpy( arr1+len1, arr2, sizeof(int)*len2);
 
     return len1+len2;
 
 }
 
-int  delete (int* arr, int len, int num)
+int  delete_ele (int* arr, int len, int num)
 {
     int position = search(arr, len, num);
 
@@ -71,7 +87,7 @@ int  delete (int* arr, int len, int num)
     else
     {
         memcpy ( arr+position, arr+position+1, sizeof(int)*(len - position) );
-        arr = realloc( arr, sizeof(int)*(len-1) );
+        arr = (int*)realloc( arr, sizeof(int)*(len-1) );
         return len-1;
     }
 }
