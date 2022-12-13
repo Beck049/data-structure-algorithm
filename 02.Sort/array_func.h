8ccfdcdf1774c9de37a7a19c4b539df78636960b
Beck049 <beckxiao2001323@gmail.com>
2022-12-13 11:09:03 +0800

    add: put array-related function together

---
+++ 02.Sort/array_func.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SWAP(x,y) {int t; t = x; x = y; y = t;} 
+
+int* cpy_array(int* origin_data, int* data, int len){
+	for(int i = 0; i < len; i++){
+		data[i] = origin_data[i];
+	}
+	return data;
+}
+
+int* init_array(int* origin, int len){
+    int* data = (int*)malloc(sizeof(int) * len);
+    data = cpy_array(origin, data, len);
+    return data;
+}
+
+void print_array(int* arr, int len){
+	for(int i = 0; i < len; i++){
+		printf("%3d ",arr[i]);
+	}
+	printf("\n");
+}
